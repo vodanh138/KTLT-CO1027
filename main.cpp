afc184363e1b46e0343bdd46157d36b639d3a576
Trương Huỳnh Anh Dũng <150664748+vodanh138@users.noreply.github.com>
2023-11-15 19:05:05 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include "knight2.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+int main(int argc, char ** argv) {
+    string file_armyknights, file_events;
+    if (argc == 1) {
+        file_armyknights = "tc1_armyknights.txt"; // hard-code
+        file_events = "tc1_events.txt";
+    }
+    else if (argc == 3) {
+        file_armyknights = argv[1];
+        file_events = argv[2];
+    }
+    else {
+        cout << "Error number of arguments" << endl;
+        exit(1);
+    }
+    
+    // BEBGIN
+    KnightAdventure * knightAdventure = new KnightAdventure();
+    knightAdventure->loadArmyKnights(file_armyknights);
+    knightAdventure->loadEvents(file_events);
+    knightAdventure->run();
+
+    delete knightAdventure;
+
+    return 0;
+}
